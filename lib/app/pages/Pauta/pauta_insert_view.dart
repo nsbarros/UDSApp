c4af1a2b8af749f143d7f70f4c5b188e7a178bcb
nsbarros <nsbarros@gmail.com.br>
2020-03-10 19:27:04 -0300

    Code refactoring

--- lib/app/pages/Pauta/pauta_insert_view.dart
+++ lib/app/pages/Pauta/pauta_insert_view.dart
@@ -23,6 +23,8 @@ class AddPauta extends View{
 class _AddPautaState extends ViewState<AddPauta, AddPautaController> {
   _AddPautaState() : super(AddPautaController(DataPautaRepository()));
 
+  final _formkey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -34,113 +36,120 @@ class _AddPautaState extends ViewState<AddPauta, AddPautaController> {
     return Scaffold(
       key: globalKey,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(
-            top: 60,
-            left: 40,
-            right: 40,
-          ),
-          child: controller.user != null ? ListView(
-            children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.text,
-                controller: widget._title,
-                validator: (value){
-                  return _validarForm(value);
-                },
-                style: new TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20),
-                decoration: InputDecoration(
-                  labelText: "Title",
-                  labelStyle: TextStyle(color: Colors.black38),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                controller: widget._description,
-                keyboardType: TextInputType.text,
-                validator: (value){
-                  return _validarForm(value);
-                },
-                style: new TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20),
-                decoration: InputDecoration(
-                  labelText: "Description",
-                  labelStyle: TextStyle(color: Colors.black38),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                enabled: false,
-                initialValue: controller.user != null ? controller.user.name : '',
-                keyboardType: TextInputType.text,
-                validator: (value){
-                  return _validarForm(value);
-                },
-                style: new TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20),
-                decoration: InputDecoration(
-                  labelText: "Autor",
-                  labelStyle: TextStyle(color: Colors.black38),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 60,
-                alignment: Alignment.centerLeft,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color.fromARGB(0, 72, 118, 255),
-                        Colors.white,
-                      ],
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(5))),
-                child: SizedBox.expand(
-                  child: FlatButton(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          "Add",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue,
-                            fontSize: 20,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                        Container(
-                          child: SizedBox(
-                            child: Icon(Icons.arrow_forward),
-                            height: 28,
-                            width: 28,
-                          ),
-                        ),
-                      ],
-                    ),
-                    onPressed: () {
-                      controller.add(Pauta('', widget._title.text, widget._description.text, widget._autor.text, 'open'));
-                    },
+        child: Form(
+          key: _formkey,
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: 60,
+              left: 40,
+              right: 40,
+            ),
+            child: controller.user != null ? ListView(
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  controller: widget._title,
+                  validator: (value){
+                    return _validarForm(value);
+                  },
+                  style: new TextStyle(
+                      color: Colors.black38,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: "Title",
+                    labelStyle: TextStyle(color: Colors.black38),
                   ),
                 ),
-              ),
-            ],
-          ) : Loading(),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: widget._description,
+                  keyboardType: TextInputType.text,
+                  validator: (value){
+                    return _validarForm(value);
+                  },
+                  style: new TextStyle(
+                      color: Colors.black38,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: "Description",
+                    labelStyle: TextStyle(color: Colors.black38),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  enabled: false,
+                  initialValue: controller.user != null ? controller.user.name : '',
+                  keyboardType: TextInputType.text,
+                  validator: (value){
+                    return _validarForm(value);
+                  },
+                  style: new TextStyle(
+                      color: Colors.black38,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: "Autor",
+                    labelStyle: TextStyle(color: Colors.black38),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 60,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color.fromARGB(0, 72, 118, 255),
+                          Colors.white,
+                        ],
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  child: SizedBox.expand(
+                    child: FlatButton(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Add",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                              fontSize: 20,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Container(
+                            child: SizedBox(
+                              child: Icon(Icons.arrow_forward),
+                              height: 28,
+                              width: 28,
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+
+                      if (_formkey.currentState.validate()) {
+                        controller.add(Pauta('', widget._title.text, widget._description.text, widget._autor.text, 'open'));
+                      }
+
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ) : Loading(),
+          ),
         ),
       ),
     );
